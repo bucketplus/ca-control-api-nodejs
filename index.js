cc1458341b21d1b3ccc67f4af6e25321aa4c03b9
kanishka <kj.kanishka@gmail.com>
2022-11-18 21:30:17 +0530

    minor fixes

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async function uploadOutputFileToKey(localPath, contenttype, key) {
   const s3 = getOutputS3();
   return await s3.putObject({
     Bucket: process.env.BP_OUTPUT_BUCKET,
-    Key: (`${process.env.BP_OUTPUT_PATH}/${key}` || `${key}`),
+    Key: (process.env.BP_OUTPUT_PATH ? `${process.env.BP_OUTPUT_PATH}/${key}` : `${key}`),
     Body: fs.readFileSync(localPath),
     ContentType: contenttype,
   }).promise();
@@ -113,7 +113,7 @@ async function getSignedOutputFileForKey(contentType, key) {
   const s3 = getOutputS3();
   return s3.getSignedUrl('putObject', {
     Bucket: process.env.BP_OUTPUT_BUCKET,
-    Key: (`${process.env.BP_OUTPUT_PATH}/${key}` || `${key}`),
+    Key: (process.env.BP_OUTPUT_PATH ? `${process.env.BP_OUTPUT_PATH}/${key}` : `${key}`),
     ContentType: contentType
   });
 }
